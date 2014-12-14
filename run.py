7b9a32608cc396dd1b145102619f04540441c744
Richard Penshorn <richard@rjpen516.net>
2014-12-14 13:47:13 -0600

    fixed import for pprint

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 import time
 import subprocess
 import os
-import pprint
+from pprint import pprint
 import glob
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
