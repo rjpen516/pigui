3831b80d9ed0c099118fe62c1d3aab809841e2bd
Richard Penshorn <richard@rjpen516.net>
2014-12-16 12:48:38 -0600

    moving labels

--- home.py
+++ home.py
@@ -8,17 +8,6 @@ from color import *
 
 from home2 import Home2
 
-output_label = None
-
-has_shown = True
-
-def show_text_task():
-    print "Hello World"
-    global has_shown
-
-    output_label.add_attribute('example2','active',has_shown)
-
-    has_shown = not has_shown
 
 
 
@@ -28,39 +17,52 @@ def show_text_task():
 
 class Home(Canvas):
     def __setup__(self, screen):
-        button = Button(screen)
-        label = Label(screen)
+        self.button = Button(screen)
+        self.label = Label(screen)
 
-        self.register_widgets(button)
-        self.register_widgets(label)
-
-
-        label.add_label('welcome', 'Welcome to Hacking Pi',125,5)
-        label.add_attribute('welcome','size',34)
-
-        label.add_label('welcome_undertext','Your Source to a touchscreen hack',125,15)
-        label.add_attribute('welcome_undertext','size',15)
-        label.add_attribute('welcome_undertext','color',red)
+        self.register_widgets(self.button)
+        self.register_widgets(self.label)
 
 
+        self.label.add_label('welcome', 'Welcome to Hacking Pi',125,5)
+        self.label.add_attribute('welcome','size',34)
 
+        self.label.add_label('welcome_undertext','Your Source to a touchscreen hacking',125,30)
+        self.label.add_attribute('welcome_undertext','size',15)
+        self.label.add_attribute('welcome_undertext','color',red)
+
+
+
+        self.label.add_label('xvalue_label', 'X:',0,100)
+        self.label.add_label('xvalue_value', '', 20,100)
+
+        self.label.add_label('yvalue_label', 'Y:',0,150)
+        self.label.add_label('yvalue_value', '', 20,150)
         
-        button.add_button('exit',show_text_task,50,50,100,100)
-        button.add_attributes('exit','color',red)
-        button.add_attributes('exit','text','EXIT')
-        button.add_button('test2',self.new_canvas_task,200,200,225,225)
-        button.add_attributes('test2','color',blue)
 
+        self.button.add_button('test2',self.move_down(),200,200,225,225)
+        self.button.add_attributes('test2','color',blue)
 
+        self.button.add_button('update_value',self.update_xy_value,0,100,40,150)
+        self.button.add_attributes('update_value','color',green)
 
 
         global output_label
-        output_label = label
+        output_label = self.label
 
-    def new_canvas_task(self):
-        home2 = Home2('home2')
-        self.runner.add_canvas('home2',home2)
-        self.runner.change_canvas('home2')
+        self.update_xy_value()
+
+
+
+    def update_xy_value(self):
+        self.label.add_attribute('xvalue_value','text',self.label.get_attribute('welcome_undertext','x'))
+        self.label.add_attribute('yvalue_value','text',self.label.get_attribute(('welcome_undertext','y')))
+
+    def move_down(self):
+        self.label.add_attribute('welcome_undertext','y',self.label.get_attribute('welcome_undertext','y')+5)
+        self.update_xy_value()
+        self.label.__render__()
+
 
 
 
--- label.py
+++ label.py
@@ -28,6 +28,9 @@ class Label(object):
     def set_render_queue(self, queue):
         self.render_queue = queue
 
+    def get_attribute(self,name,type):
+        return self.attributes[name][type]
+
 
     def __render__(self):
         for key in self.labels:
