925190e4ea9630bdf0ae1f71aee9ab5a4d4f9bb8
Richard Penshorn <richard@rjpen516.net>
2014-12-16 15:24:44 -0600

    added more debugging

--- move_label.py
+++ move_label.py
@@ -56,8 +56,9 @@ class MoveLabel(Widget):
 
     def set_render_queue(self, queue):
         print "MoveLabel Widget"
-        self.button.render_queue = queue
-        self.label.render_queue = queue
+        print queue
+        self.button.set_render_queue(queue)
+        self.label.set_render_queue(queue)
         super(MoveLabel,self).set_render_queue(queue)
 
 
