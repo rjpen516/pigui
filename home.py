c77ed848a2bd466aa33c40c770ed9c5b830c0f56
Richard Penshorn <richard@rjpen516.net>
2014-12-16 16:38:18 -0600

    fixed style

--- home.py
+++ home.py
@@ -33,9 +33,9 @@ class Home(Canvas):
         self.label.add_attribute('welcome_undertext','color',red)
 
 
-        self.button.add_button('show_editor',self.make_moving_form_visable,0,self.runner.size[1]-25,25,self.runner.size[1])
+        self.button.add_button('show_editor',self.make_moving_form_visable,0,self.runner.size[1]-25,75,self.runner.size[1])
         self.button.add_attributes('show_editor','text','Show Move')
-        self.button.add_attributes('show_editor','size',10)
+        self.button.add_attributes('show_editor','text_size',25)
 
         self.move_label = MoveLabel(screen,'welcome_undertext',self.label, visable=False)
         self.register_widgets(self.move_label)
